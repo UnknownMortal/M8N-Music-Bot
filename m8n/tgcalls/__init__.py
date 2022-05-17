8b8f6b2abd46b654e42f3ebc88a529a0d9fce6ea
Unknown Mortal <104091630+UnknownMortal@users.noreply.github.com>
2022-05-17 12:10:39 +0530

    Create __init__.py

---
+++ m8n/tgcalls/__init__.py
@@ -0,0 +1,13 @@
+from os import listdir, mkdir
+from pyrogram import Client
+from m8n import config
+from m8n.tgcalls.queues import clear, get, is_empty, put, task_done
+from m8n.tgcalls import queues
+from m8n.tgcalls.youtube import download
+from m8n.tgcalls.calls import run, pytgcalls
+from m8n.tgcalls.calls import client
+
+if "raw_files" not in listdir():
+    mkdir("raw_files")
+
+from oda.tgcalls.convert import convert
