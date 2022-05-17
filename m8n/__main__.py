40d7991ca375ef405350845f06992b723165aeb9
Unknown Mortal <104091630+UnknownMortal@users.noreply.github.com>
2022-05-17 11:53:01 +0530

    Create __main__.py

---
+++ m8n/__main__.py
@@ -0,0 +1,58 @@
+import asyncio
+import requests
+from pyrogram import Client
+from pytgcalls import idle
+from m8n import app
+from m8n.database.functions import clean_restart_stage
+from m8n.database.queue import get_active_chats, remove_active_chat
+from m8n.tgcalls.calls import run
+from m8n.config import API_ID, API_HASH, BOT_TOKEN, BG_IMG
+
+
+response = requests.get(BG_IMG)
+with open("./etc/foreground.png", "wb") as file:
+    file.write(response.content)
+
+
+async def load_start():
+    restart_data = await clean_restart_stage()
+    if restart_data:
+        print("[INFO]: SENDING RESTART STATUS")
+        try:
+            await app.edit_message_text(
+                restart_data["chat_id"],
+                restart_data["message_id"],
+                "**Restarted the Bot Successfully.**",
+            )
+        except Exception:
+            pass
+    served_chats = []
+    try:
+        chats = await get_active_chats()
+        for chat in chats:
+            served_chats.append(int(chat["chat_id"]))
+    except Exception as e:
+        print("Error came while clearing db")
+    for served_chat in served_chats:
+        try:
+            await remove_active_chat(served_chat)
+        except Exception as e:
+            print("Error came while clearing db")
+            pass
+    print("[INFO]: STARTED")
+
+
+loop = asyncio.get_event_loop_policy().get_event_loop()
+loop.run_until_complete(load_start())
+
+Client(
+    ":memory:",
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins={"root": "m8n.modules"},
+).start()
+
+run()
+idle()
+loop.close()
