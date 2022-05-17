fd0ac16feae84f560442b6c47993097b9832d670
Unknown Mortal <104091630+UnknownMortal@users.noreply.github.com>
2022-05-17 15:02:28 +0530

    Update config.py

--- m8n/config.py
+++ m8n/config.py
@@ -1,1 +1,33 @@
+# Created By @Its_romeoo
+# Copyright By M8N
 
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+que = {}
+admins = {}
+SESSION_NAME = getenv("SESSION_NAME", "session")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME")
+BOT_USERNAME = getenv("BOT_USERNAME")
+ASSID = int(getenv("ASSID"))
+ASSNAME = getenv("ASSNAME")
+ASSUSERNAME = getenv("ASSUSERNAME")
+BOT_ID = int(getenv("BOT_ID"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+MONGO_DB_URI = getenv("MONGO_DB_URI")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+OWNER_ID = int(getenv("OWNER_ID"))
+UPDATE = getenv("UPDATE", "M8N_OFFICIAL")
+SUPPORT = getenv("SUPPORT", "M8N_SUPPORT")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "999"))
+CMD_MUSIC = list(getenv("CMD_MUSIC", "/ !").split())
+BG_IMG = getenv("BG_IMG")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
