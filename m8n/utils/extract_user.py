b7f7171d3ab1f4921a900e63b0229a8f3dec9166
Unknown Mortal <104091630+UnknownMortal@users.noreply.github.com>
2022-05-18 14:16:00 +0530

    Create extract_user.py

---
+++ m8n/utils/extract_user.py
@@ -0,0 +1,57 @@
+from pyrogram.types import Message
+from pyrogram.types import User
+from datetime import datetime
+
+
+def extract_user(message: Message) -> (int, str):
+    user_id = None
+    user_first_name = None
+
+    if message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+        user_first_name = message.reply_to_message.from_user.first_name
+
+    elif len(message.command) > 1:
+        if (
+            len(message.entities) > 1 and
+            message.entities[1].type == "text_mention"
+        ):
+            # 0: is the command used
+            # 1: should be the user specified
+            required_entity = message.entities[1]
+            user_id = required_entity.user.id
+            user_first_name = required_entity.user.first_name
+        else:
+            user_id = message.command[1]
+            # don't want to make a request -_-
+            user_first_name = user_id
+
+        try:
+            user_id = int(user_id)
+        except ValueError:
+            print("Scam ")
+
+    else:
+        user_id = message.from_user.id
+        user_first_name = message.from_user.first_name
+
+    return (user_id, user_first_name)
+
+
+def last_online(from_user: User) -> str:
+    time = ""
+    if from_user.is_bot:
+        time += "🤖 Bot :("
+    elif from_user.status == 'recently':
+        time += "Recently"
+    elif from_user.status == 'within_week':
+        time += "Within the last week"
+    elif from_user.status == 'within_month':
+        time += "Within the last month"
+    elif from_user.status == 'long_time_ago':
+        time += "A long time ago :("
+    elif from_user.status == 'online':
+        time += "Currently Online"
+    elif from_user.status == 'offline':
+        time += datetime.fromtimestamp(from_user.last_online_date).strftime("%a, %d %b %Y, %H:%M:%S")
+    return time
