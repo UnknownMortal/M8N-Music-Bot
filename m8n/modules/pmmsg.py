9f14ec4b4838fba9e3905516303d616be5557e23
Unknown Mortal <104091630+UnknownMortal@users.noreply.github.com>
2022-05-26 03:19:47 +0530

    Create pmmsg.py

---
+++ m8n/modules/pmmsg.py
@@ -0,0 +1,15 @@
+# Copyright (©️) @M8N_OFFICIAL
+# By : Pavan Magar
+
+from pyrogram import Client
+from Codexun.tgcalls import client as USER
+from pyrogram import filters
+from pyrogram.types import Chat, Message, User
+from Codexun.config import (
+    BOT_USERNAME,
+)
+
+@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
+async def pmPermit(client: USER, message: Message):
+  await USER.send_message(message.chat.id,"Hey 👋 I am the assistant of music bot, didn't have a time to talk with you 🙂 kindly join @M8N_SUPPORT for getting support\n\nPowered by @M8N_OFFICIAL")
+  return
