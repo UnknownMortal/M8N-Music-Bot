0c1eb61f26458630e3b15114b102c16ececc7925
Unknown Mortal <104091630+UnknownMortal@users.noreply.github.com>
2022-05-27 12:42:37 +0530

    Update play.py

--- m8n/modules/play.py
+++ m8n/modules/play.py
@@ -314,7 +314,7 @@ async def play(_, message: Message):
         file_name = get_file_name(audio)
         url = f"https://t.me/{UPDATE}"
         title = audio.title
-        thumb_name = "https://telegra.ph/file/a7adee6cf365d74734c5d.png"
+        thumb_name = "https://telegra.ph//file/461807815346e07838083.jpg"
         thumbnail = thumb_name
         duration = round(audio.duration / 60)
         views = "Locally added"
@@ -367,7 +367,7 @@ async def play(_, message: Message):
 
         except Exception as e:
             title = "NaN"
-            thumb_name = "https://telegra.ph/file/a7adee6cf365d74734c5d.png"
+            thumb_name = "https://telegra.ph//file/461807815346e07838083.jpg"
             duration = "NaN"
             views = "NaN"
             keyboard = InlineKeyboardMarkup(
