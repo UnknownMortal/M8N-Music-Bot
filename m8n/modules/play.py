c44d6c41b6f5234335f492a62f3fbf6fa274b898
Unknown Mortal <104091630+UnknownMortal@users.noreply.github.com>
2022-06-05 15:09:57 +0530

    Update play.py

--- m8n/modules/play.py
+++ m8n/modules/play.py
@@ -569,7 +569,7 @@ async def play(_, message: Message):
         await message.reply_photo(
             photo="final.png",
             reply_markup=keyboard,
-            caption="**[Get Additional Information 💡]({})**\n\n**⑆ User :** **{}**\n**⑆ Group : [{}..](https://t.me/codexun)**".format(
+            caption="**[Get Additional Information 💡]({})**\n\n**⑆ User :** **{}**\n**⑆ Group : [{}](https://t.me/M8N_OFFICIAL)**".format(
                 url, message.from_user.mention(), message.chat.title
             ),
         )
@@ -595,7 +595,7 @@ async def play(_, message: Message):
         await message.reply_photo(
             photo="final.png",
             reply_markup=keyboard,
-            caption="**[Get Additional Information 💡]({})**\n\n**⑆ User :** **{}**\n**⑆ Group : [{}..](https://t.me/codexun)**".format(
+            caption="**[Get Additional Information 💡]({})**\n\n**⑆ User :** **{}**\n**⑆ Group : [{}](https://t.me/M8N_OFFICIAL)**".format(
                 url, message.from_user.mention(), message.chat.title
             ),
         )
